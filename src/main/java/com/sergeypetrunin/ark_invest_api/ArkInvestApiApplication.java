45301903983137742a0a0b8f4aae6e385d14fc74
Sergey Petrunin <halfstrik@gmail.com>
2026-08-11 20:28:55 -0400

    Initial commit Newly downloaded minimalistic Sprin Boot app v4.1.0. Using Java 21, seems to be the best choise. Using Maven as I'm most comfortable with it.

---
+++ src/main/java/com/sergeypetrunin/ark_invest_api/ArkInvestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergeypetrunin.ark_invest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkInvestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkInvestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergeypetrunin/ark_invest_api/ArkInvestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeypetrunin.ark_invest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkInvestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
