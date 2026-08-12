445f104df00641e0e3d9106a35c02f93fdb6f123
Sergey Petrunin <halfstrik@gmail.com>
2026-08-11 21:16:33 -0400

    Added simple /health endpoint just to see API working Added simple test method for /health endpoint Added api-test.http collection Added simple README.md file with brif project description

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ark Invest API - take home task
+
+The goal is to create simple but production ready CRUD API using Spring Boot and Java
+
+To run server using Maven:
+```shell
+./mvnw spring-boot:run
+```
+
+To run whole application with DB locally using Docker:
+```shell
+docker compose up -d # TODO
+```
---
+++ src/main/java/com/sergeypetrunin/arkinvest/controllers/HealthController.java
@@ -0,0 +1,11 @@
+package com.sergeypetrunin.arkinvest.controllers;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "API is healthy";
+    }
+}
