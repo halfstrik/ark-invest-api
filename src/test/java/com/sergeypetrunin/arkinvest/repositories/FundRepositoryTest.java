30488a65e2ef75d7908466a309ca363a29477f75
Sergey Petrunin <halfstrik@gmail.com>
2026-08-14 17:57:14 -0400

    Added tests with Sqlite both for repository and for /funds endpoint

---
+++ src/test/java/com/sergeypetrunin/arkinvest/repositories/FundRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.sergeypetrunin.arkinvest.repositories;
+
+import com.sergeypetrunin.arkinvest.models.Fund;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FundRepositoryTest {
+
+    @Autowired
+    FundRepository repository;
+
+    @Autowired
+    JdbcClient jdbc;
+
+    @BeforeEach
+    void cleanDatabase() {
+        jdbc.sql("DELETE FROM fund").update();
+    }
+
+    @Test
+    void shouldSaveAndFindFund() {
+        var fund = new Fund(
+                UUID.randomUUID(),
+                "Retirement Fund",
+                "Test fund"
+        );
+        jdbc.sql("""
+            INSERT INTO fund (id, name, description)
+            VALUES (:id, :name, :desc)
+            """)
+                .param("id", fund.id().toString())
+                .param("name", fund.name())
+                .param("desc", fund.description())
+                .update();
+
+        var result = repository.findAll();
+
+        assertThat(result).containsExactly(fund);
+    }
+
+}
