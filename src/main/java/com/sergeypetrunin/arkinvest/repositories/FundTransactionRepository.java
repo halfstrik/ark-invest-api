b1353a71d3790eb8431194a11894e6e96ee221c4
Sergey Petrunin <halfstrik@gmail.com>
2026-08-30 10:57:54 -0400

    Make API work, a few tests still fails WIP

--- src/main/java/com/sergeypetrunin/arkinvest/repositories/FundTransactionRepository.java
+++ src/main/java/com/sergeypetrunin/arkinvest/repositories/FundTransactionRepository.java
@@ -48,7 +48,7 @@ public class FundTransactionRepository {
                 TransactionType.valueOf(rs.getString("transaction_type")),
                 TransactionEffect.valueOf(rs.getString("transaction_effect")),
                 rs.getBigDecimal("amount"),
-                Instant.parse(rs.getString("transaction_date")),
+                rs.getTimestamp("transaction_date").toInstant(),
                 rs.getString("description")
         );
     }
